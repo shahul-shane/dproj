4c67c3769ee819aed33cf2e7210ed845503246ae
shahul-shane <84654179+shahul-shane@users.noreply.github.com>
2022-04-06 12:54:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -20,6 +20,27 @@
    setTimeout(showSlides, 2000); // Change image every 2 seconds
  }
 
+ let slideIndex1 = 0;
+ showSlides1();
+ 
+ function showSlides1() {
+   let i;
+   let slides = document.getElementsByClassName("mySlides1");
+   let dots = document.getElementsByClassName("dot1");
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";  
+   }
+   slideIndex1++;
+   if (slideIndex1 > slides.length) {slideIndex1 = 1}    
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex1-1].style.display = "block";  
+   dots[slideIndex1-1].className += " active";
+   setTimeout(showSlides1, 2000); // Change image every 2 seconds
+ }
+
+
  function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
